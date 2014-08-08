230ad1ef1908f90766df135df1df58861b2864e4
kaplanme <kaplanme@umich.edu>
2014-08-08 16:12:07 -0400

    So close! we only need to solve the system

---
+++ Fortran/GMC/Debug/inverse__genmod.f90
@@ -0,0 +1,10 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Fri Aug 08 16:00:30 2014
+        MODULE INVERSE__genmod
+          INTERFACE 
+            SUBROUTINE INVERSE(A,C,N)
+              INTEGER(KIND=4) :: N
+              REAL(KIND=8) :: A(N,N)
+              REAL(KIND=8) :: C(N,N)
+            END SUBROUTINE INVERSE
+          END INTERFACE 
+        END MODULE INVERSE__genmod
--- Fortran/GMC/GMC2.f90
+++ Fortran/GMC/GMC2.f90
@@ -12,7 +12,7 @@
 !
 !****************************************************************************   
 PROGRAM GMC
-
+    EXTERNAL DGESV
     IMPLICIT NONE
 
     ! Variables
@@ -35,8 +35,10 @@ PROGRAM GMC
     REAL, DIMENSION(7,7) :: SM
     INTEGER              :: Ng, Nb, g, b
     ! Solving Variables
-    DOUBLE PRECISION, DIMENSION(:, :), ALLOCATABLE :: A, K, M, A_
-    INTEGER :: count = 1, i
+    DOUBLE PRECISION, DIMENSION(:, :), ALLOCATABLE :: A, K, M
+	INTEGER, ALLOCATABLE :: IPIV(:)
+    INTEGER :: count = 1, i, NRHS, LDA, LDB, N, INFO
+	DOUBLE PRECISION, 
     
     
 !-----------------------------------------------------------------------------
@@ -99,11 +101,15 @@ PROGRAM GMC
 !------------------------------------------------------------------------------
 !                             BUILD SOLUTION
 !------------------------------------------------------------------------------
+    N = Ng*Nb*6
+    ALLOCATE(A(N,N))
+    ALLOCATE(K(N,6))
+    ALLOCATE(M(N,6))
+    ALLOCATE(IPIV(N))
     
-    ALLOCATE(A(Ng*Nb*6,Ng*Nb*6))
-    ALLOCATE(A_(Ng*Nb*6,Ng*Nb*6))
-    ALLOCATE(K(Ng*Nb*6,6))
-    ALLOCATE(M(Ng*Nb*6,6))
+    NRHS = 6
+    LDA = N
+    LDB = N
     
     A = 0.0
     K = 0.0
@@ -278,90 +284,21 @@ END DO
 !------------------------------------------------------------------------------
 !                                   SOLVE
 !------------------------------------------------------------------------------
-A_ = A
-CALL inverse(A_,M,Ng*Nb*6)
 
+!CALL inverse(A_,M,Ng*Nb*6)
+M = K
+WRITE (*,*) N
+WRITE (*,*) NRHS
+WRITE (*,*) LDA
+WRITE (*,*) LDB
+WRITE (*,*) SIZE(A,1), SIZE(A,2)
+WRITE (*,*) SIZE(M,1), SIZE(M,2)
+
+
+!call DGESV( N, NRHS, A, LDA, IPIV, M, LDB, INFO )
+
+Write (*,*) INFO
     PAUSE
     END PROGRAM GMC
 
-subroutine inverse(a,c,n)
-!============================================================
-! Inverse matrix
-! Method: Based on Doolittle LU factorization for Ax=b
-! Alex G. December 2009
-!-----------------------------------------------------------
-! input ...
-! a(n,n) - array of coefficients for matrix A
-! n      - dimension
-! output ...
-! c(n,n) - inverse matrix of A
-! comments ...
-! the original matrix a(n,n) will be destroyed 
-! during the calculation
-!===========================================================
-implicit none 
-integer :: n
-double precision :: a(n,n)
-double precision :: c(n,n)
-double precision :: L(n,n), U(n,n), b(n), d(n), x(n)
-double precision :: coeff
-integer :: i, j, k
-
-! step 0: initialization for matrices L and U and b
-! Fortran 90/95 allows such operations on matrices
-L=0.0
-U=0.0
-b=0.0
-
-! step 1: forward elimination
-do k=1, n-1
-   do i=k+1,n
-      coeff=a(i,k)/a(k,k)
-      L(i,k) = coeff
-      do j=k+1,n
-         a(i,j) = a(i,j)-coeff*a(k,j)
-      end do
-   end do
-end do
-
-! Step 2: prepare L and U matrices 
-! L matrix is a matrix of the elimination coefficient
-! + the diagonal elements are 1.0
-do i=1,n
-  L(i,i) = 1.0
-end do
-! U matrix is the upper triangular part of A
-do j=1,n
-  do i=1,j
-    U(i,j) = a(i,j)
-  end do
-end do
-
-! Step 3: compute columns of the inverse matrix C
-do k=1,n
-  b(k)=1.0
-  d(1) = b(1)
-! Step 3a: Solve Ld=b using the forward substitution
-  do i=2,n
-    d(i)=b(i)
-    do j=1,i-1
-      d(i) = d(i) - L(i,j)*d(j)
-    end do
-  end do
-! Step 3b: Solve Ux=d using the back substitution
-  x(n)=d(n)/U(n,n)
-  do i = n-1,1,-1
-    x(i) = d(i)
-    do j=n,i+1,-1
-      x(i)=x(i)-U(i,j)*x(j)
-    end do
-    x(i) = x(i)/u(i,i)
-  end do
-! Step 3c: fill the solutions x(n) into column k of C
-  do i=1,n
-    c(i,k) = x(i)
-  end do
-  b(k)=0.0
-end do
-end subroutine inverse
 
